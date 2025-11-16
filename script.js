cfd703c225f76e5c823015cc0debd92aa8d96b64
sisema <sisema07@gmail.com>
2025-11-16 14:38:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1023,7 +1023,7 @@ function drawPassportImage(parque, atividade, userUploadedPhoto) {
         const badgeSize = 450; // Tamanho (estava "perfeito")
         const rotationAngle = -15 * Math.PI / 180; // Rotação (estava "perfeito")
         // AJUSTE: "metade do badge sobre a ponta superior da foto"
-        const badgeX = photoX - (badgeSize / 3.5); // Sobrepõe um pouco a esquerda da foto
+        const badgeX = photoX - (badgeSize / 3); // Sobrepõe um pouco a esquerda da foto
         const badgeY = photoY - (badgeSize / 2); // Sobrepõe metade para cima da foto
 
         // 4. TEXTOS (Check-in, Parque, Badge)
@@ -1443,3 +1443,4 @@ function iniciarApp() {
 }
 
 document.addEventListener('DOMContentLoaded', inicializar);
+
